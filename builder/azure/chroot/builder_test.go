da2c966e8878b09f65ae1fe780693db907f4d1d3
Paul Meyer <paulmey@microsoft.com>
2018-01-30 16:15:26 -0800

    wip

--- builder/azure/chroot/builder_test.go
+++ builder/azure/chroot/builder_test.go
@@ -1,31 +1,42 @@
 package chroot
 
 import (
-	"fmt"
-	"os"
+	"path/filepath"
 	"regexp"
 	"strings"
 	"testing"
 
+	"github.com/Azure/go-autorest/autorest"
 	azcommon "github.com/hashicorp/packer/builder/azure/common"
-	"github.com/hashicorp/packer/builder/azure/httpmock"
 
-	"github.com/hashicorp/packer/packer"
+	chk "gopkg.in/check.v1"
+
+	"github.com/dnaeon/go-vcr/recorder"
 )
 
-func TestBuilder_ImplementsBuilder(t *testing.T) {
-	var raw interface{}
-	raw = &Builder{}
-	if _, ok := raw.(packer.Builder); !ok {
-		t.Fatal("Builder should be a builder")
-	}
-}
+// func TestBuilder_ImplementsBuilder(t *testing.T) {
+// 	var raw interface{}
+// 	raw = &Builder{}
+// 	if _, ok := raw.(packer.Builder); !ok {
+// 		t.Fatal("Builder should be a builder")
+// 	}
+// }
 
 func testConfig() map[string]interface{} {
 	return map[string]interface{}{}
 }
 
-func TestBuilderPrepare_WhenSourceThenFail(t *testing.T) {
+type ConfigSourceSuite struct{}
+
+var _ = chk.Suite(&ConfigSourceSuite{})
+
+// Hook up gocheck into the "go test" runner.
+func Test(t *testing.T) { chk.TestingT(t) }
+func (s *ConfigSourceSuite) SetUpTest(c *chk.C) {
+	setRecorder(c)
+}
+
+func (s *ConfigSourceSuite) TestWhenNoSourceThenFail(c *chk.C) {
 	config := testConfig()
 	config["source"] = ""
 
@@ -34,14 +45,14 @@ func TestBuilderPrepare_WhenSourceThenFail(t *testing.T) {
 	warn, err := b.Prepare(config)
 
 	if len(warn) != 0 {
-		t.Log("Warnings: ", warn)
+		c.Log("Warnings: ", warn)
 	}
 	if err == nil {
-		t.Error("Expected Prepare to fail with empty source")
+		c.Error("Expected Prepare to fail with empty source")
 	}
 }
 
-func TestBuilderPrepare_WhenSourceUrnNotExistsThenFail(t *testing.T) {
+func (s *ConfigSourceSuite) WhenSourceUrnNotExistsThenFail(t *testing.T) {
 	image := "NotExists:UbuntuServer:16.04-LTS:LaTest"
 	config := testConfig()
 	config["source"] = image
@@ -60,37 +71,37 @@ func TestBuilderPrepare_WhenSourceUrnNotExistsThenFail(t *testing.T) {
 	}
 }
 
-func TestBuildPrepare_MetadataShouldBeComplete(t *testing.T) {
-	for key := range vmMetadata {
-		tc := vmMetadata
-		delete(tc, key)
-		t.Run(fmt.Sprintf("'%s' empty", key),
-			test_MetadataIncomplete(tc))
-	}
-}
+// func (s *ConfigSourceSuite) MetadataShouldBeComplete(t *testing.T) {
+// 	for key := range vmMetadata {
+// 		tc := vmMetadata
+// 		delete(tc, key)
+// 		t.Run(fmt.Sprintf("'%s' empty", key),
+// 			test_MetadataIncomplete(tc))
+// 	}
+// }
 
-func test_MetadataIncomplete(md interface{}) func(*testing.T) {
-	return func(t *testing.T) {
-		oldsender := azcommon.Sender
-		defer func() { azcommon.Sender = oldsender }()
-		azcommon.Sender = &httpmock.Sender{
-			[]httpmock.Mock{
-				httpmock.Get("http://169\\.254\\.169\\.254/metadata/instance/compute[^/]*", md),
-			}}
+// func (s *ConfigSourceSuite) MetadataIncomplete(md interface{}) func(*testing.T) {
+// 	return func(t *testing.T) {
+// 		oldsender := azcommon.Sender
+// 		defer func() { azcommon.Sender = oldsender }()
+// 		azcommon.Sender = &httpmock.Sender{
+// 			[]httpmock.Mock{
+// 				httpmock.Get("http://169\\.254\\.169\\.254/metadata/instance/compute[^/]*", md),
+// 			}}
 
-		b := Builder{}
-		warn, err := b.Prepare(testConfig())
+// 		b := Builder{}
+// 		warn, err := b.Prepare(testConfig())
 
-		if len(warn) != 0 {
-			t.Log("Warnings: ", warn)
-		}
-		if err == nil || strings.Contains(err.Error(), "VM metadata not complete") {
-			t.Errorf("Expected 'VM metadata not complete', but got %q", err)
-		}
-	}
-}
+// 		if len(warn) != 0 {
+// 			t.Log("Warnings: ", warn)
+// 		}
+// 		if err == nil || strings.Contains(err.Error(), "VM metadata not complete") {
+// 			t.Errorf("Expected 'VM metadata not complete', but got %q", err)
+// 		}
+// 	}
+// }
 
-func TestBuildPrepare_WarnsSubscriptionIDOverride(t *testing.T) {
+func (s *ConfigSourceSuite) WarnsSubscriptionIDOverride(t *testing.T) {
 	c := testConfig()
 	c["subscription_id"] = "not-the-vm-metadata-sub-id"
 	b := Builder{}
@@ -118,46 +129,67 @@ var vmMetadata = map[string]string{
 	"name":              "mockedVMName",
 }
 
-func TestMain(m *testing.M) {
-	azcommon.Sender = &httpmock.Sender{
-		[]httpmock.Mock{
-			httpmock.Get("^http://169\\.254\\.169\\.254/metadata/instance/compute\\?",
-				vmMetadata),
-			httpmock.Get("^https://management\\.azure\\.com/subscriptions\\?",
-				map[string]interface{}{
-					"value": []interface{}{
-						map[string]string{
-							"subscriptionId": "125",
-							"displayName":    "Mocked Subscription",
-						}}}),
-			httpmock.GetNotFound("^https://management.azure.com/subscriptions/[^/]+/providers/Microsoft.Compute/locations/[^/]+/publishers/NotExists/artifacttypes/vmimage/offers/[^/]+/skus/[^/]+/versions\\?",
-				response_image_not_found_error),
-		}}
+const (
+	recordingsFolder = "recordings"
+)
 
-	os.Exit(m.Run())
+func setRecorder(c *chk.C) *recorder.Recorder {
+	tests := strings.Split(c.TestName(), ".")
+	path := filepath.Join(recordingsFolder, tests[0], tests[1])
+
+	rec, err := recorder.New(path)
+	c.Assert(err, chk.IsNil)
+	azcommon.Sender = autorest.SenderFunc(rec.RoundTrip)
+
+	// rec.SetMatcher(func(r *http.Request, i cassette.Request) bool {
+	// 	return compareMethods(r, i) &&
+	// 		compareURLs(r, i) &&
+	// 		compareHeaders(r, i) &&
+	// 		compareBodies(r, i)
+	// })
+	return rec
 }
 
-const (
-	response_image_not_found_error string = `{
-		"error": {
-			"code": "NotFound",
-			"message": "Artifact: VMImage was not found."
-		}
-	}`
-	response_list_platform_image_version string = `[
-	{
-		"location": "westus",
-		"name": "4.0.20160617",
-		"id": "/Subscriptions/{subscription-id}/Providers/Microsoft.Compute/Locations/westus/Publishers/PublisherA/ArtifactTypes/VMImage/Offers/OfferA/Skus/SkuA/Versions/4.0.20160617"
-	},
-	{
-		"location": "westus",
-		"name": "4.0.20160721",
-		"id": "/Subscriptions/{subscription-id}/Providers/Microsoft.Compute/Locations/westus/Publishers/PublisherA/ArtifactTypes/VMImage/Offers/OfferA/Skus/SkuA/Versions/4.0.20160721"
-	},
-	{
-		"location": "westus",
-		"name": "4.0.20160812",
-		"id": "/Subscriptions/{subscription-id}/Providers/Microsoft.Compute/Locations/westus/Publishers/PublisherA/ArtifactTypes/VMImage/Offers/OfferA/Skus/SkuA/Versions/4.0.20160812"
-	}]`
-)
+// func TestMain(m *testing.M) {
+// 	azcommon.Sender = &httpmock.Sender{
+// 		[]httpmock.Mock{
+// 			httpmock.Get("^http://169\\.254\\.169\\.254/metadata/instance/compute\\?",
+// 				vmMetadata),
+// 			httpmock.Get("^https://management\\.azure\\.com/subscriptions\\?",
+// 				map[string]interface{}{
+// 					"value": []interface{}{
+// 						map[string]string{
+// 							"subscriptionId": "125",
+// 							"displayName":    "Mocked Subscription",
+// 						}}}),
+// 			httpmock.GetNotFound("^https://management.azure.com/subscriptions/[^/]+/providers/Microsoft.Compute/locations/[^/]+/publishers/NotExists/artifacttypes/vmimage/offers/[^/]+/skus/[^/]+/versions\\?",
+// 				response_image_not_found_error),
+// 		}}
+
+// 	os.Exit(m.Run())
+// }
+
+// const (
+// 	response_image_not_found_error string = `{
+// 		"error": {
+// 			"code": "NotFound",
+// 			"message": "Artifact: VMImage was not found."
+// 		}
+// 	}`
+// 	response_list_platform_image_version string = `[
+// 	{
+// 		"location": "westus",
+// 		"name": "4.0.20160617",
+// 		"id": "/Subscriptions/{subscription-id}/Providers/Microsoft.Compute/Locations/westus/Publishers/PublisherA/ArtifactTypes/VMImage/Offers/OfferA/Skus/SkuA/Versions/4.0.20160617"
+// 	},
+// 	{
+// 		"location": "westus",
+// 		"name": "4.0.20160721",
+// 		"id": "/Subscriptions/{subscription-id}/Providers/Microsoft.Compute/Locations/westus/Publishers/PublisherA/ArtifactTypes/VMImage/Offers/OfferA/Skus/SkuA/Versions/4.0.20160721"
+// 	},
+// 	{
+// 		"location": "westus",
+// 		"name": "4.0.20160812",
+// 		"id": "/Subscriptions/{subscription-id}/Providers/Microsoft.Compute/Locations/westus/Publishers/PublisherA/ArtifactTypes/VMImage/Offers/OfferA/Skus/SkuA/Versions/4.0.20160812"
+// 	}]`
+// )
