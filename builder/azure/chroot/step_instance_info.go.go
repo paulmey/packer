bf09ae0c688bb468058d86b8c5500429d29f35c5
Paul Meyer <paulmey@microsoft.com>
2019-05-27 06:19:50 +0000

    add instance info step

---
+++ builder/azure/chroot/step_instance_info.go.go
@@ -0,0 +1,42 @@
+package chroot
+
+import (
+	"context"
+	"fmt"
+	"github.com/hashicorp/packer/builder/azure/chroot/client"
+	"github.com/hashicorp/packer/helper/multistep"
+	"github.com/hashicorp/packer/packer"
+	"log"
+)
+
+var _ multistep.Step = &StepInstanceInfo{}
+
+type StepInstanceInfo struct {
+}
+
+func (StepInstanceInfo) Run(ctx context.Context, state multistep.StateBag) multistep.StepAction {
+	azcli := state.Get("azureclient").(client.AzureClientSet)
+	ui := state.Get("ui").(packer.Ui)
+
+	ui.Say("Gathering information about this Azure VM...")
+
+	info, err := azcli.MetadataClient().GetComputeInfo()
+	if err != nil {
+		log.Printf("StepInstanceInfo: error: %+v", err)
+		err := fmt.Errorf(
+			"Error retrieving information ARM resource ID and location" +
+				"of the VM that Packer is running on.\n" +
+				"Please verify that Packer is running on a proper Azure VM.")
+		state.Put("error", err)
+		ui.Error(err.Error())
+		return multistep.ActionHalt
+	}
+	log.Printf("StepInstanceInfo: ID: %s", info.ResourceID())
+	log.Printf("StepInstanceInfo: location: %s", info.Location)
+
+	state.Put("instance", info)
+
+	return multistep.ActionContinue
+}
+
+func (StepInstanceInfo) Cleanup(multistep.StateBag) {}
