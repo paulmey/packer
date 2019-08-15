be6c2614e3b2d043252e2c64271f8ba05e77b1f7
Paul Meyer <paulmey@microsoft.com>
2019-08-15 20:05:03 +0000

    Remove govcr dependency
    Still need to fake the attaching of the disk at the filesystem level,
    so making this a pure integration scenario

--- builder/azure/common/client/testclient.go
+++ builder/azure/common/client/testclient.go
@@ -1,50 +1,31 @@
 package client
 
 import (
-	"github.com/Azure/go-autorest/autorest/azure/auth"
-	"github.com/seborama/govcr"
 	"os"
 	"testing"
+	"net/http"
+	"errors"
+
+	"github.com/Azure/go-autorest/autorest/azure/auth"
 )
 
 func GetTestClientSet(t *testing.T) (AzureClientSet, error) {
-	replayOnly :=
-		os.Getenv("AZURE_CLIENT_ID") == "" ||
-			os.Getenv("AZURE_CLIENT_SECRET") == "" ||
-			os.Getenv("AZURE_SUBSCRIPTION_ID") == "" ||
-			os.Getenv("AZURE_TENANT_ID") == ""
-
-	cfg := &govcr.VCRConfig{
-		DisableRecording: true,
-		RequestFilters: govcr.RequestFilters{
-			govcr.RequestDeleteHeaderKeys("Authorization"),
-			govcr.RequestFilter(func(req govcr.Request) govcr.Request {
-				req.URL.Path = subscriptionPathRegex.ReplaceAllLiteralString(req.URL.Path,
-					"/subscriptions/00000000-0000-1234-0000-000000000000")
-				return req
-			}).OnPath(`/management.azure.com/`),
-		},
-	}
-	cli := azureClientSet{}
-
-	if replayOnly {
-		t.Log("Azure credentials not available, will use existing recordings.")
-		cli.subscriptionID = "00000000-0000-1234-0000-000000000000"
+	if (os.Getenv("AZURE_INTEGRATION_TEST") == "") {
+		t.Skip("AZURE_INTEGRATION_TEST not set")		
 	} else {
-		if os.Getenv("AZURE_RECORD") != "" {
-			cfg.DisableRecording = false
-			cfg.Logging = true
-		}
 		a, err := auth.NewAuthorizerFromEnvironment()
 		if err == nil {
-			cli.authorizer = a
+			cli := azureClientSet{}
+		cli.authorizer = a
 			cli.subscriptionID = os.Getenv("AZURE_SUBSCRIPTION_ID")
 			cli.PollingDelay = 0
+			cli.sender = http.DefaultClient
+			return cli, nil
+					} else {
+			t.Skipf("Could not create Azure client: %v", err)
 		}
 	}
 
-	vcr := govcr.NewVCR(t.Name(), cfg)
-	cli.sender = vcr.Client
-	return cli, nil
+	return nil, errors.New("Couldn't create client set")
 }
 
